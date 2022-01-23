088cbc2ab750a5ab78350599b6432d8338b5ba8c
Andre <andre_arias.29@hotmail.com>
2022-01-23 09:46:17 -0300

    3º Commit: Criando navbar e footer

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
 import Contact from './components/pages/Contact';
+import Projects from './components/pages/Projects';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer'
+
 
 function App() {
   return (
     <Router>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Company</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </ul>
+      <Navbar/>
       <Switch>
         <Container customClass="min-height">
           <Route exact path="/"> <Home/> </Route>
+          <Route exact path="/projects"> <Projects/> </Route>
           <Route exact path="/company"> <Company/> </Route>
           <Route exact path="/contact"> <Contact/> </Route>
           <Route exact path="/newproject"> <NewProject/> </Route>
         </Container>
       </Switch>
-      <p>Footer</p>
+      <Footer/>
     </Router>
   );
 }
