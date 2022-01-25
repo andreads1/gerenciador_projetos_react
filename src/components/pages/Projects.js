5694b44cea0817e9cd2d9c4cd6187655393f2cc4
Andre <andre_arias.29@hotmail.com>
2022-01-25 19:34:31 -0300

    12º Commit: Implementando remoção de projetos e mensagem ao excluir

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -15,6 +15,7 @@ function Projects (){
 
     const [projects, setProjects] = useState([])
     const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] =useState('')
 
     const location = useLocation()
     let message = ''
@@ -41,6 +42,23 @@ function Projects (){
             },300)
     },[])
 
+    function removeProject(id){
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method:'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+
+        })
+        .then((resp)=> resp.json())
+        .then(()=> {
+            setProjects(projects.filter((project)=> project.id !== id))
+            //mensagem de remoção
+            setProjectMessage('Projeto removido com sucesso!')
+        })
+        .catch((err)=> console.log(err))
+    }
+
     return (
         <div className={styles.project_container}>
             <div className={styles.title_container}>
@@ -48,6 +66,7 @@ function Projects (){
                 <LinkButton to ="/newproject" text="Criar projeto"/>
             </div>
             {message && <Message type="success" msg={message}/>}
+            {projectMessage && <Message type="success" msg={projectMessage}/>}
             <Container customClass="start">
                 {projects.length>0 &&
                     projects.map((project)=> (
@@ -56,7 +75,8 @@ function Projects (){
                         name={project.name}
                         budget={project.budget}
                         category={project.category.name}
-                        key={project.id}/>
+                        key={project.id}
+                        handleRemove={removeProject}/>
                     ))
                 }
                 {!removeLoading && <Loading/>}
